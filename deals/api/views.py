fa695f95e625aa7a1c261fbbda57ecb9dd47f211
Soufiane Chalouh <schalouh@leyton.com>
2020-11-01 21:31:39 +0100

    Add deals model, urls.py, views and serializer

---
+++ deals/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework import status, viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from deals.models import Deal
+from deals.api.serializers import DealSerializer
+
+
+class DealsViewSet(viewsets.ModelViewSet):
+    queryset = Deal.objects.all()
+    permissions = [
+        permissions.AllowAny
+    ]
+    serializer_class = DealSerializer
