5a523be386917e3e528803b53048d6865c077147
soufiane chalouh <soufianechalouh@gmail.com>
2020-12-07 00:03:08 +0100

    fix requirement + commit necessary file

--- deals/views.py
+++ deals/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+import csv
+from django.http import HttpResponse
 
-# Create your views here.
+
+def get_deals(request):
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="deals.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
+    writer.writerow(['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
+
+    return response
