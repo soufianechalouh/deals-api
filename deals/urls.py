218c4d031295426d0a08bf5bb38b9e296861b88c
soufiane chalouh <soufianechalouh@gmail.com>
2020-12-26 11:35:28 +0100

    reduce routes to publish working Beta: Only serving CSVs

--- deals/urls.py
+++ deals/urls.py
@@ -1,14 +1,9 @@
-from django.urls import path
 from rest_framework import routers
-from . import views
 
 from deals.api.views import DealsViewSet
 
 router = routers.DefaultRouter()
 router.register('deals', DealsViewSet, 'deals')
 
-urlpatterns = [
-    path('csv_deals', views.get_deals, name='deals-csv')
-]
-urlpatterns += router.urls
+urlpatterns = router.urls
 
