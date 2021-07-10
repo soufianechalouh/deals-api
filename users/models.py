6ddc65e3068ed6267b0554c772e4227aea81f279
soufiane chalouh <soufianechalouh@gmail.com>
2021-07-10 21:21:07 +0100

    create UserManager with create_user

--- users/models.py
+++ users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, AbstractBaseUser, BaseUserManager
 from django.db import models
 
 # Create your models here.
@@ -6,7 +6,21 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy
 
 
-class User(AbstractBaseUser):
+class UserManager(BaseUserManager):
+    def create_user(self, email, user_name, first_name, password, **other_fields):
+
+        if not email:
+            raise ValueError(gettext_lazy("Every user should have an Email"))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, user_name=user_name, first_name=first_name, **other_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(gettext_lazy("email address"), unique=True)
     user_name = models.CharField(max_length=150, unique=True)
     first_name = models.CharField(max_length=150, blank=True)
