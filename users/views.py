7c3ce7b3670bb89d15d07ee98e753d199bc7d3c1
soufiane chalouh <soufianechalouh@gmail.com>
2021-07-11 23:56:35 +0100

    FEA: Created user registration view and linked it to urls

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, status
+from rest_framework.response import Response
+
+from users.serializers import RegisterSerializer
+
+
+class RegisterView(generics.GenericAPIView):
+
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        user = request.data
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        user_data = serializer.data
+
+        return Response(user_data, status=status.HTTP_201_CREATED)
