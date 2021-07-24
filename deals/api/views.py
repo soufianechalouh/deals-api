0bc4ab564cba9b18789c13fcecdf76c09ccbd511
soufiane chalouh <soufianechalouh@gmail.com>
2021-07-24 01:12:03 +0100

    feat: Set permissions so authenticated user can read and only staff can add or update

--- deals/api/views.py
+++ deals/api/views.py
@@ -2,11 +2,14 @@ from datetime import datetime, timedelta
 
 from rest_framework import viewsets, permissions
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 from deals.models import Deal
 from deals.api.serializers import DealSerializer
 
 from rest_framework_csv.renderers import CSVRenderer
+
+from deals.permissions import IsStaffOrReadOnly
 
 
 class DealsViewSet(viewsets.ModelViewSet):
@@ -20,6 +23,7 @@ class DealsViewSet(viewsets.ModelViewSet):
 
 
 class AllDealsViewSet(DealsViewSet):
+    permission_classes = [IsAuthenticated & IsStaffOrReadOnly]
     queryset = Deal.objects.all()
 
 
